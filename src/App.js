444cb00fe28429169d8c1b6399db19ad59a9c888
fransschuijt <fp.schuijt@gmail.com>
2023-04-01 13:07:24 +0200

    opdracht af

--- src/App.js
+++ src/App.js
@@ -1,12 +1,92 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Maak hier jouw formulier!
-    </div>
-  );
+    const [formState, setFormState] = useState({
+        name: "",
+        age: 0,
+        comments: "",
+        newsletter: false,
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+    function handleChange(e) {
+        const value = e.target.name;
+        const newValue = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+        setFormState({
+            ...formState,
+            [value]: newValue,
+        });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>Gegevens</legend>
+
+                <label htmlFor="details-name">
+                    Naam:
+                    <input
+                        type="text"
+                        name="name"
+                        id="details-name"
+                        value={formState.name}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label htmlFor="details-age">
+                    Leeftijd:
+                    <input
+                        type="number"
+                        name="age"
+                        id="details-age"
+                        value={formState.age}
+                        onChange={handleChange}
+                    />
+                </label>
+            </fieldset>
+
+            <fieldset>
+                <legend>Jouw review</legend>
+
+                <label htmlFor="review-text">
+                    Opmerkingen:
+                    <textarea
+                        placeholder="Wat vond je van het recept?"
+                        rows="4"
+                        cols="40"
+                        name="comments"
+                        id="review-text"
+                        value={formState.comments}
+                        onChange={handleChange}
+                    >
+                    </textarea>
+                </label>
+
+                <label htmlFor="newsletter-checkbox">
+                    <input
+                        type="checkbox"
+                        name="newsletter"
+                        id="newsletter-checkbox"
+                        checked={formState.newsletter}
+                        onChange={handleChange}
+                    />
+                    Ik schrijf me in voor de nieuwsbrief
+                </label>
+
+                <button type="submit">
+                    Versturen
+                </button>
+
+            </fieldset>
+
+        </form>
+    );
 }
 
 export default App;
